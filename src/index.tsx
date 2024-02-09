6fcfc31f4603570ed53f4c450e167b00fe1e61af
Pedro Cruz <pedrovictor.lage@gmail.com>
2024-02-09 17:22:51 -0300

    fix: update component import

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { View, StyleSheet } from 'react-native';
 
 let SaffeCaptureComponent: any;
+
 const loadComponent = async () => {
   const module = await require('./SaffeCaptureComponent');
   SaffeCaptureComponent = module.default;
@@ -11,7 +12,7 @@ loadComponent();
 export default function SaffeCapture(props: any) {
   return (
     <View style={styles.container}>
-      <SaffeCaptureComponents
+      <SaffeCaptureComponent
         onError={props.onError}
         onLoad={props.onLoad}
         apiKey={props.apiKey}
